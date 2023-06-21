f95847841dd3bfd4df9420074e57fe95ce20bf02
thitipan-suwannnakhot <136154153+thitipan-suwannnakhot@users.noreply.github.com>
2023-06-21 20:40:12 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Thitipan Suwannakhot</span>
+
+        </div>
+    );
+}
+
+export default Footer;
